2ab3194f202300f0a3c59bbc2b0b68694678d5e0
akita8 <marko-b@hotmail.it>
2016-07-31 11:26:39 +0200

    minor fixes

--- scrapper.py
+++ scrapper.py
@@ -6,6 +6,7 @@ from random import randint
 from time import sleep
 from email.mime.text import MIMEText
 from subprocess import call
+import datetime
 
 
 
@@ -23,12 +24,11 @@ def get_last_price(isin):
 def send_email(text):
 
     msg = MIMEText(text)
-    msg['Subject'] = 'pi'
+    msg['Subject'] = 'notifica bond {}'.format(datetime.datetime.today())
     msg['From'] = 'cavolo9876@gmail.com'
     msg['To'] = 'cavolo9876@gmail.com'
     s = smtplib.SMTP('localhost')
     s.send_message(msg)
-    print('messagio inviato')
     s.quit()
 
 def check(diz):
@@ -37,13 +37,11 @@ def check(diz):
     msg=''
 
     for bond in diz:
-        print('aggiorno {}'.format(bond))
         price=get_last_price(diz[bond][0])
         if price < float(diz[bond][1]):
             notification=True
             text='{0} è sceso sotto la soglia di {1}, ultimo prezzo {2}'.format(bond, diz[bond][1], price)
             msg+=text+'\n'
-            print(text)
         sleep(randint(15, 30))
     if notification:
         send_email(msg)
@@ -55,25 +53,20 @@ def get_bonds(filename):
         bonds={x[0]:(x[1], x[2]) for x in formatted}
     return bonds
 
-def mainloop():
+def main():
 
     try:
-        while True:
-            call(['git', 'pull'])
-            check(get_bonds('bonds.csv'))
-            print('standby')
-            sleep(1200)
 
-    except KeyboardInterrupt:
-        mainloop()
+        check(get_bonds('bonds.csv'))
+
+
     except:
-        err =  sys.exc_info()[1]
-        msg='errore programma pi: {}'.format(err)
-        print(msg)
+        err =  sys.exc_info()[0]
+        msg='errore programma: {}'.format(err)
         send_email(msg)
 
 
 
 
 if __name__ == '__main__':
-    mainloop()
+    main()
